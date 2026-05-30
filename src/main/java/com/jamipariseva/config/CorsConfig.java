8ab4857dd5f574e6b6685e9be81492eca5e34059
susmi-cse <162762272+susmi-cse@users.noreply.github.com>
2026-05-30 21:15:37 +0530

    Add CORS configuration and database properties
    Added configuration properties for server and PostgreSQL datasource.

--- src/main/java/com/jamipariseva/config/CorsConfig.java
+++ src/main/java/com/jamipariseva/config/CorsConfig.java
@@ -1,4 +1,11 @@
-package com.jamipariseva.config;
 
-public class CorsConfig {
-}
+server.port=8080
+
+
+spring.datasource.url=jdbc:postgresql://localhost:5432/jamipariseva_db
+spring.datasource.username=postgres
+spring.datasource.password=your_postgres_password
+spring.datasource.driver-class-name=org.postgresql.Driver
+
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
